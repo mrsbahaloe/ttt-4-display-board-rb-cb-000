e0f281252dbcb90be99914b4e190477f2a02a3d8
Claudia Kenny <claudiaskenny@gmail.com>
2017-11-06 17:23:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,9 @@
-def display_board(board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "])
-  puts "#{board[0]}""|#{board[1]}|""#{board[2]}"
+def display_board(board = ["o", "x", " ", " ", "x", " ", "x", "o", " "])
+  puts "#{board[0]}"|"#{board[1]}"|"#{board[2]}"
   puts "-----------"
-  puts "#{board[3]}""|#{board[4]}|""#{board[5]}"
+  puts "#{board[3]}"|"#{board[4]}"|"#{board[5]}"
   puts "-----------"
-  puts "#{board[7]}""|#{board[8]}|""#{board[9]}"
+  puts "#{board[7]}"|"#{board[8]}"|"#{board[9]}"
 end
 display_board
+
